e2c6aaf30d5320ca43304dfab7c380f5ef02f22f
poonamjoshi30 <34467387+poonamjoshi30@users.noreply.github.com>
2022-08-12 01:33:03 +0300

    [Feature][Main]Added theme

--- app/src/main/java/com/example/cryptocurrency/MainActivity.kt
+++ app/src/main/java/com/example/cryptocurrency/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.cryptocurrency.presentation
+package com.example.cryptocurrency
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
@@ -8,7 +8,7 @@ import androidx.compose.material.Surface
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.cryptocurrency.R
+import com.example.cryptocurrency.presentation.Screen
 import com.example.cryptocurrency.presentation.coin_detail.CoinDetailScreen
 import com.example.cryptocurrency.presentation.coin_list.CoinListScreen
 import com.plcoding.cryptocurrency.ui.theme.CryptocurrencyAPPTheme
